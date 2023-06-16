71af31394d135fdd7f0ecd841b20a7ff8b35e685
Rebecca Stevens <rebecca.stevens@outlook.co.nz>
2023-06-17 05:55:56 +1200

    refactor: use a type file to load vite client types

---
+++ src/vite-env.d.ts
@@ -0,0 +1,6 @@
+/// <reference types="vite-plugin-pages/client" />
+/// <reference types="vite-plugin-pwa/client" />
+/// <reference types="vite-plugin-vue-layouts/client" />
+/// <reference types="vite/client" />
+/// <reference types="vitest" />
+/// <reference types="vue/ref-macros" />
